ef585914be2455d91773ab388989505421e11aaa
marksmerritt <markmerritt804@gmail.com>
2022-12-31 12:26:51 -0500

    Initial commit

---
+++ lib/stockr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "stockr/version"
+
+module Stockr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/stockr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Stockr
+  VERSION = "0.1.0"
+end
