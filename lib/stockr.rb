6fb54f655565e3d05788729529dcc67717821f62
marksmerritt <markmerritt804@gmail.com>
2023-01-01 14:48:35 -0500

    wip

--- lib/stockr.rb
+++ lib/stockr.rb
@@ -4,5 +4,20 @@ require_relative "stockr/version"
 
 module Stockr
   autoload :Client, "stockr/client"
+  autoload :Collection, "stockr/collection"
   autoload :Error, "stockr/error"
+  autoload :Object, "stockr/object"
+
+  module TimeSeries
+    autoload :Intraday, "stockr/objects/time_series/intraday"
+  end
+
+  module Intelligence
+  end
+
+  module Fundamentals
+  end
+
+  module Forex
+  end
 end
